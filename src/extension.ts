58274fb93a4567fb5cce0fa4b0d298b10e9b6cab
Yeison Daza <yeison@tesorio.com>
2021-08-22 17:57:00 -0500

    add configuration

--- src/extension.ts
+++ src/extension.ts
@@ -5,15 +5,27 @@ import * as isColor from 'is-color';
 import { getValueKind, createCompletionItem } from './utils';
 
 type Var = { name: string, value: string };
+type Config = {
+  include: vscode.GlobPattern
+  exclude: vscode.GlobPattern | undefined
+  autoCompleteOn: vscode.DocumentSelector
+};
 
 export async function activate(context: vscode.ExtensionContext) {
   console.log('Congratulations, your extension "styled-global-variable-autocomplete" is now active!');
-  const filesUri = await vscode.workspace.findFiles('**/*.globals.{ts,js}', undefined);
+  const config = vscode.workspace.getConfiguration('variablesAutocomplete');
+  const includeFilesGlob = config.get<Config['include']>('include', '**/*.globals.{ts,js}');
+  const excludeFilesGlob = config.get<Config['exclude']>('exclude');
+  const autoCompleteOn = config.get<Config['autoCompleteOn']>('autoCompleteOn', ['javascript', 'typescript']);
 
+  // Find all files that match configured criteria
+  const filesUri = await vscode.workspace.findFiles(includeFilesGlob, excludeFilesGlob);
+
+  // Read all found files
   const filesPromises = filesUri.map(file => vscode.workspace.openTextDocument(file).then(document => document.getText()));
-
   const files = await Promise.all(filesPromises);
 
+  // Get all variables from lines
   const finalItems = files.flatMap(file => {
     const vars = new Map();
 
@@ -32,7 +44,7 @@ export async function activate(context: vscode.ExtensionContext) {
       .filter(Boolean) as Var[];
   });
 
-  const provider = vscode.languages.registerCompletionItemProvider(['javascript', 'typescript'], {
+  const provider = vscode.languages.registerCompletionItemProvider(autoCompleteOn, {
     provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken) {
       const range = document.getWordRangeAtPosition(position, /\S+/);
       const currentLine = document.lineAt(position.line);
