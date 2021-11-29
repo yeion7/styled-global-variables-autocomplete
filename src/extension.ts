15b2972ad20794ebd276c9651b832228e05ff6e8
Jamie Martin (FE Platform) <74973140+marti6jm@users.noreply.github.com>
2021-11-29 05:08:29 -0500

    Updates to Autocomplete Triggering (#2)

--- src/extension.ts
+++ src/extension.ts
@@ -1,106 +1,171 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import { getCompletionItem, getHoverPreview, getVariableAtPosition } from './utils';
+import {
+  getCompletionItem,
+  getHoverPreview,
+  getVariableAtPosition,
+} from './utils';
 
-type Var = { name: string, value: string, uri: vscode.Uri, range: vscode.Range };
+type Var = {
+  name: string;
+  value: string;
+  uri: vscode.Uri;
+  range: vscode.Range;
+};
 type Config = {
-  include: vscode.GlobPattern
-  exclude: vscode.GlobPattern | undefined
-  autoCompleteOn: vscode.DocumentSelector
+  include: vscode.GlobPattern;
+  exclude: vscode.GlobPattern | undefined;
+  autoCompleteOn: vscode.DocumentSelector;
 };
 
-const defaultConfig: Config  = {
+const defaultConfig: Config = {
   include: '**/*.globals.{ts,js,tsx,jsx}',
   exclude: undefined,
-  autoCompleteOn: ['javascript', 'typescript', 'javascriptreact', 'typescriptreact']
+  autoCompleteOn: [
+    'javascript',
+    'typescript',
+    'javascriptreact',
+    'typescriptreact',
+  ],
 };
 
 export async function activate(context: vscode.ExtensionContext) {
-  console.log('Congratulations, your extension "styled-global-variable-autocomplete" is now active!');
+  console.log(
+    'Congratulations, your extension "styled-global-variable-autocomplete" is now active!'
+  );
   const config = vscode.workspace.getConfiguration('variablesAutocomplete');
-  const includeFilesGlob = config.get<Config['include']>('include', defaultConfig.include);
-  const excludeFilesGlob = config.get<Config['exclude']>('exclude', defaultConfig.exclude);
-  const autoCompleteOn = config.get<Config['autoCompleteOn']>('autoCompleteOn', defaultConfig.autoCompleteOn);
+  const includeFilesGlob = config.get<Config['include']>(
+    'include',
+    defaultConfig.include
+  );
+  const excludeFilesGlob = config.get<Config['exclude']>(
+    'exclude',
+    defaultConfig.exclude
+  );
+  const autoCompleteOn = config.get<Config['autoCompleteOn']>(
+    'autoCompleteOn',
+    defaultConfig.autoCompleteOn
+  );
 
   // Find all files that match configured criteria
-  const filesUri = await vscode.workspace.findFiles(includeFilesGlob, excludeFilesGlob);
+  const filesUri = await vscode.workspace.findFiles(
+    includeFilesGlob,
+    excludeFilesGlob
+  );
 
   // Read all found files
-  const filesPromises = filesUri.map(file => vscode.workspace.openTextDocument(file));
+  const filesPromises = filesUri.map((file) =>
+    vscode.workspace.openTextDocument(file)
+  );
   const documents = await Promise.all(filesPromises);
 
   // Get all variables from lines
-  const finalItems = documents.flatMap(document => {
+  const finalItems = documents.flatMap((document) => {
     const file = document.getText();
     const vars = new Map();
 
-    return file.split(/\r?\n/)
+    return file
+      .split(/\r?\n/)
       .map((line, i) => {
         const lineTrim = line.trim();
         const isVariable = line.trim().startsWith('--');
-        if (!isVariable) { return; };
+        if (!isVariable) {
+          return;
+        }
 
-        const [name, rawValue] = lineTrim.split(":");
+        const [name, rawValue] = lineTrim.split(':');
         const value = rawValue.trim().replace(';', '');
 
         // Prevent duplicate or empty variables
-        if (!value || vars.has(name)) { return; };
+        if (!value || vars.has(name)) {
+          return;
+        }
 
         vars.set(name, value);
         return {
           name,
           value,
           uri: document.uri,
-          range: new vscode.Range(new vscode.Position(i, 4), new vscode.Position(i, name.length + 4))
+          range: new vscode.Range(
+            new vscode.Position(i, 4),
+            new vscode.Position(i, name.length + 4)
+          ),
         } as Var;
       })
       .filter(Boolean) as Var[];
   });
 
   // Support autocomplete
-  const completionProvider = vscode.languages.registerCompletionItemProvider(autoCompleteOn, {
-    provideCompletionItems(document: vscode.TextDocument, position: vscode.Position) {
-      const range = document.getWordRangeAtPosition(position, /\S+/);
-      const currentLine = document.lineAt(position.line);
-      const isCssPropLine = currentLine.text.includes('css={{');
+  const completionProvider = vscode.languages.registerCompletionItemProvider(
+    autoCompleteOn,
+    {
+      provideCompletionItems(
+        document: vscode.TextDocument,
+        position: vscode.Position,
+        token,
+        context
+      ) {
+        const range = document.getWordRangeAtPosition(position, /\S+/);
+        const currentLine = document.lineAt(position.line);
+        const isCssPropLine = currentLine.text.includes('css={{');
+        const isVarPresent = currentLine.text.includes('var(');
 
-      const variables = finalItems.map(({ name, value }) => getCompletionItem({
-        name,
-        value,
-        range,
-        isCssPropLine
-      }));
+        const variables = finalItems.map(({ name, value }) =>
+          getCompletionItem({
+            name,
+            value,
+            range,
+            isCssPropLine,
+            isVarPresent,
+          })
+        );
 
-      return variables;
+        return variables;
+      },
     }
-  });
+  );
 
   // Support go to definition
-  const definitionProvider = vscode.languages.registerDefinitionProvider(autoCompleteOn, {
-    async provideDefinition(document: vscode.TextDocument, position: vscode.Position) {
-      const variable = getVariableAtPosition(document, position);
+  const definitionProvider = vscode.languages.registerDefinitionProvider(
+    autoCompleteOn,
+    {
+      async provideDefinition(
+        document: vscode.TextDocument,
+        position: vscode.Position
+      ) {
+        const variable = getVariableAtPosition(document, position);
 
-      if (!variable) { return []; };
+        if (!variable) {
+          return [];
+        }
 
-      return finalItems
-        .filter(({ name }) => variable === name)
-        .map(({ name, uri, range }) => new vscode.Location(uri, range));
+        return finalItems
+          .filter(({ name }) => variable === name)
+          .map(({ name, uri, range }) => new vscode.Location(uri, range));
+      },
     }
-  });
+  );
 
   const hoverProvider = vscode.languages.registerHoverProvider(autoCompleteOn, {
-    async provideHover(document: vscode.TextDocument, position: vscode.Position) {
+    async provideHover(
+      document: vscode.TextDocument,
+      position: vscode.Position
+    ) {
       const variable = getVariableAtPosition(document, position);
 
-      if (!variable) { return null; };
+      if (!variable) {
+        return null;
+      }
 
       const variableItem = finalItems.find(({ name }) => name === variable);
 
-      if (!variableItem) { return null; };
+      if (!variableItem) {
+        return null;
+      }
 
       return new vscode.Hover(getHoverPreview(variableItem.value));
-    }
+    },
   });
 
   context.subscriptions.push(completionProvider);
